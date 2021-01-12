d6711a19eaeaf1feb2a9950487665fe3cecf9cdc
Luís Rodrigues <luis.cnm@gmail.com>
2021-01-12 15:13:41 +0000

    Added draw grid methods
    manhattan distance util to figure out distances

--- main.py
+++ main.py
@@ -77,3 +77,38 @@ class Pixel:
     
     def __lt__(self, other):
         return False
+
+# manhatthan distance
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+# square grid
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            pixel = Pixel(i, j, gap, rows)
+            grid[i].append(pixel)
+    
+    return grid
+
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+# main draw function
+def draw(win, grid, rows, width):
+    win.fill(WHITE)
+    for row in grid:
+        for pixel in row:
+            pixel.draw(win)
+
+    draw_grid(win, rows, width)
+    pygame.display.update()
