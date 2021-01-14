55771ba4c0a5e95d33b9bba514383a2031007445
Luís Rodrigues <luis.cnm@gmail.com>
2021-01-14 14:37:15 +0000

    Finished main visualizer
    - Added grid clearing
    - Added optimal path visualization
    - Needs refactoring

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def h(p1, p2):
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
 
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.paint_path()
+        draw()
+
 # a* algorithm
 def algorithm_astar(draw, grid, start, end):
     count = 0
@@ -37,6 +43,8 @@ def algorithm_astar(draw, grid, start, end):
         open_set_hash.remove(current)
 
         if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.paint_end() 
             return True
         
         for n in current.neighbors:
@@ -102,11 +110,11 @@ def main(win, width):
     ROWS = 50
     grid = make_grid(ROWS, width)
 
+    # start and end nodes
     start = None
     end = None
     
     run = True
-    started = False
 
     while run:
         draw(win, grid, ROWS, width)
@@ -114,9 +122,6 @@ def main(win, width):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
-            if started:
-                continue
 
             if pygame.mouse.get_pressed()[0]: # left mouse button
                 pos = pygame.mouse.get_pos()
@@ -142,13 +147,16 @@ def main(win, width):
                     end = None
             
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end: # start algorithm
                     for row in grid:
                         for pixel in row:
                             pixel.update_neighbors(grid)
                     
                     algorithm_astar(lambda: draw(win, grid, ROWS, width), grid, start, end)
-    
+                if event.key == pygame.K_c:                     # clear grid
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
     pygame.quit()
 
 main(WIN, WIDTH)
